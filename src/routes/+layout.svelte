1098fd2dd103a3166b946aa3b253a05868d15918
Pavithran Chelliahpillai <pavithranchelliahpillai@Pavithrans-MacBook-Pro.local>
2024-07-28 09:19:55 -0400

    preload commands

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,48 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
   <link rel="preload" as="video" href="/bgvid.mp4">
+  <link rel="preload" as="image" href="/af.webp">
+  <link rel="preload" as="image" href="/aquablue.webp">
+  <link rel="preload" as="image" href="/aspenridge.webp">
+  <link rel="preload" as="image" href="/at.webp">
+  <link rel="preload" as="image" href="/best.webp">
+  <link rel="preload" as="image" href="/bglogo.webp">
+  <link rel="preload" as="image" href="/calendar.webp">
+  <link rel="preload" as="image" href="/cat.webp">
+  <link rel="preload" as="image" href="/clock.webp">
+  <link rel="preload" as="image" href="/comm.webp">
+  <link rel="preload" as="image" href="/commercial.webp">
+  <link rel="preload" as="image" href="/downsview.webp">
+  <link rel="preload" as="image" href="/dup.webp">
+  <link rel="preload" as="image" href="/ecobee.png">
+  <link rel="preload" as="image" href="/effi.webp">
+  <link rel="preload" as="image" href="/fa.webp">
+  <link rel="preload" as="image" href="/fair.webp">
+  <link rel="preload" as="image" href="/favicon.webp">
+  <link rel="preload" as="image" href="/graywood.webp">
+  <link rel="preload" as="image" href="/icon.webp">
+  <link rel="preload" as="image" href="/industrial.webp">
+  <link rel="preload" as="image" href="/licese.webp">
+  <link rel="preload" as="image" href="/mainlineimg.png">
+  <link rel="preload" as="image" href="/matera.webp">
+  <link rel="preload" as="image" href="/menu.webp">
+  <link rel="preload" as="image" href="/pc.webp">
+  <link rel="preload" as="image" href="/pizzaville.webp">
+  <link rel="preload" as="image" href="/porcello.webp">
+  <link rel="preload" as="image" href="/pricipleairlogo.webp">
+  <link rel="preload" as="image" href="/proj1.jpg">
+  <link rel="preload" as="image" href="/proj2.jpg">
+  <link rel="preload" as="image" href="/proj3.jpg">
+  <link rel="preload" as="image" href="/proj4.jpg">
+  <link rel="preload" as="image" href="/proj5.jpg">
+  <link rel="preload" as="image" href="/proj6.jpg">
+  <link rel="preload" as="image" href="/quality.webp">
+  <link rel="preload" as="image" href="/residential.webp">
+  <link rel="preload" as="image" href="/si.webp">
+  <link rel="preload" as="image" href="/solp.webp">
+  <link rel="preload" as="image" href="/van.webp">
+  <link rel="preload" as="image" href="/vegfresh.webp">
+  <link rel="preload" as="image" href="/von.webp">
   
   <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100..900;1,100..900&display=swap">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100..900;1,100..900&display=swap">
