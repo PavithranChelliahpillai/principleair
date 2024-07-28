703e6da9b02f255b64d833a7dc1dd4d17474b0df
Pavithran Chelliahpillai <pavithranchelliahpillai@Pavithrans-MacBook-Pro.local>
2024-07-28 09:11:50 -0400

    efficient web files + preloads

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,22 @@
 <script>
     import '../app.css'
 </script>
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+  <link rel="preload" as="video" href="/bgvid.mp4">
+  
+  <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100..900;1,100..900&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100..900;1,100..900&display=swap">
+  
+  <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap">
+  
+  <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap">
+</svelte:head>
   
 <slot />
   
